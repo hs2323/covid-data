2cd01291a63296a1729f5bde89394235a9b2d0de
hs2323 <hs2323@gmail.com>
2021-09-12 15:09:23 -0400

    Adding default setup from Sample Module Repository

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# covid-data
+# COVID Data
 Read data from an Excel file, grab API data and write it to a new Excel sheet.
---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='sample',
+    version='0.0.1',
+    description='Get COVID data from an API',
+    long_description=readme,
+    author='Harry Shyket',
+    author_email='',
+    url='https://github.com/hs2323/covid-data',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
